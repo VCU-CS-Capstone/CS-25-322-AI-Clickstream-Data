1d299c54e1c82adfa07ae4cfae14642bb061ae37
trcrsa <trieuc3@vcu.edu>
2025-01-19 15:01:55 -0500

    redirect script

---
+++ src/Redirect.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Redirect() {
+  return (
+    <div className="redirect-page">
+      <h2>More coming soon</h2>
+    </div>
+  );
+}
+
+export default Redirect;
