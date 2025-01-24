ac6aa21b8a1c12023f9c0d4d08d6a7b1069dcb46
trcrsa <trieuc3@vcu.edu>
2025-01-24 11:25:33 -0500

    track redirect page

--- src/RedirectedPage.js
+++ src/RedirectedPage.js
@@ -3,8 +3,18 @@ import { Link } from 'react-router-dom';
 import './RedirectedPage.css';
 import logo from './assets/logo.png';
 import bannerImage from './assets/banner.png';
+import ReactGA from 'react-ga4';
 
 const RedirectedPage = () => {
+
+  const handleBackButtonClick = () => {
+    ReactGA.event({
+      category: 'Button Click',
+      action: 'Go Back to Home Page',
+      label: 'Redirected Page - Home Button',
+    });
+  };
+
   return (
     <div className="App">
       {/* Header Section */}
@@ -21,9 +31,9 @@ const RedirectedPage = () => {
           <a href="mailto:support@yourbank.com" className="icon-button">
             <img src={require('./assets/search.png')} alt="Email" className="icon-image" />
           </a>
-          <a href="/support" className="icon-button">
+          <Link to="/redirect" className="icon-button" onClick={() => handleButtonClick('Support Icon')}>
             <img src={require('./assets/person.png')} alt="Support" className="icon-image" />
-          </a>
+          </Link>
         </div>
       </div>
 
@@ -37,7 +47,7 @@ const RedirectedPage = () => {
         <h2>You have been redirected!</h2>
         <p>Please return to the home page for assistance.</p>
         <Link to="/">
-          <button className="back-button">Go Back to Home Page</button>
+          <button className="back-button" onClick={handleBackButtonClick}>Go Back to Home Page</button>
         </Link>
       </div>
     </div>
