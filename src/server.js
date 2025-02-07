2c5f7ede8a57344203fe240f7d48b50f1b85a1ee
trcrsa <trieuc3@vcu.edu>
2025-02-07 09:40:47 -0500

    import statement

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import fs from 'fs';
 import { BetaAnalyticsDataClient } from '@google-analytics/data';
 
 dotenv.config();
