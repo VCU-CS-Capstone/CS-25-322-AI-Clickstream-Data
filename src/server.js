78d863b7bacb435538618aeabbe4728c0e2d16e4
trcrsa <trieuc3@vcu.edu>
2025-04-23 12:47:35 -0400

    update cors

--- src/server.js
+++ src/server.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(cors({origin: '*'}));
 
+app.use(cors({
+  origin: 'https://cs-25-322-ai-clickstream-data.vercel.app',
+  methods: ['GET', 'POST'],
+  credentials: true 
+}));
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Success"))
   .catch((err) => {
